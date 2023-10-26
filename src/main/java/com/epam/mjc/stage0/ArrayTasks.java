dba77cff1feadcfa5850eed0ef93edaf63b5c240
Нодир Асомов <nasomov93@gmail.com>
2023-10-26 20:36:40 +0500

    hi

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -7,151 +7,149 @@ package com.epam.mjc.stage0;
  */
 public class ArrayTasks {
 
-    /**
-     * Return a String[] array that will list all the seasons of the year, starting with winter.
-     */
-    public String[] seasonsArray() {
 
-        String[] seasons = new String[4];
+    public static void main(String[] args) {
+        ArrayTasks arrayTasks= new ArrayTasks();
+        arrayTasks.seasonsArray();
+        arrayTasks.generateNumbers(5);
+    }
+
+        public String[] seasonsArray() {
+
+        String[] seasons=new String[5];
         seasons[0] = " ";
         seasons[1] = "winter";
         seasons[2] = "spring";
         seasons[3] = "summer";
         seasons[4] = "autumn";
-        //System.out.println(seasons);
-        for (int i=1;i<4;i++){
+        for (int i = 1; i < 5; i++) {
             System.out.println(seasons[i]);
+        }
+              return seasonsArray();}
+
+    public int[] generateNumbers(int length) { int[] intArr = {1,2,3,4,5,6};
+        for (int num : intArr) {
+            for (int i = 1; i <=length; i++) {
+                //System.out.print();
+                System.out.print("[");
+                for (int j = 1; j <=i; j++) {
+                    System.out.print(j+",");
+                }
+                System.out.print("]");
+                System.out.println();
+            }
+        }
+        return generateNumbers(5);
+    }
+
+    public Object[] reverseArray(String[] arr) {
+        Integer[] intArray = {10,20,30,40,50,60,70,80,90};
+
+        //print array starting from first element
+        System.out.println("Original Array:");
+        for(int i=0;i<intArray.length;i++)
+            System.out.print(intArray[i] + "  ");
+
+        System.out.println();
+
+        //print array starting from last element
+        System.out.println("Original Array printed in reverse order:");
+        for(int i=intArray.length-1;i>=0;i--)
+            System.out.print(intArray[i] + "  ");
+        return reverseArray(arr);
+    }
+
+    public int totalSum(int[] arr) {
+         arr = new int [] {1, 2, 3, 4, 5};
+        int sum = 0;
+        //Loop through the array to calculate sum of elements
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            //System.out.println(sum);
+        }
+        System.out.println(sum);
+        return totalSum(arr);
+    }
+
+
+
+
+    public int findIndexOfNumber(int[] arr, int number) {
+        int target=102;
+        int[] numbers = {99, -7, 102};
+        for (int index = 0; index < numbers.length; index++) {
+        if (numbers[index] == target) {
+        return index;
+        }
+        }
+        return -1;
         }
 
 
 
-
-
-
-
-
-    /**
-     * Generate an int[] array of consecutive positive integers
-     * starting at 1 of the given length (length parameter > 0).
-     * <p>
-     * Example:
-     * <p>
-     * length = 1  -> [1]
-     * length = 3  -> [1, 2, 3]
-     * length = 5  -> [1, 2, 3, 4, 5]
-     */
-
-
-
-    public  int[] generateNumbers(int length){
-
-
-             int [] intArr= {1,2,3,4,5,6};
-             int lenth =intArr[length];
-            for (int i : intArr) {
-                for ( i = 1; i <=5; i++) {
-                    //System.out.print();
-                    System.out.print("[");
-                    for (int j = 1; j <=i; j++) {
-                        System.out.print(j+",");
-                    }
-                    System.out.print("]");
-                    System.out.println();
-                }
-            }}
-    };
-
-       /* int[] intArr = {1,2,3,4,5,6};
-        for (int num : intArr) {
-            System.out.print("[");
-            for(int i=0;i<num;i++){
-                System.out.print(num+",");
-
-            }
-            System.out.print("]");
-            System.out.println();
-        }*/
-
-
-
-
-
-
-
-
-
-    /**
-     * Find the sum of all elements of the int[] array.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1, 3, 5]   -> sum = 9
-     * arr = [5, -3, -4] -> sum = -2
-     */
-    public int totalSum(int[] arr) {
-
-        return 0;}
-
-    /**
-     * Return the index of the first occurrence of number in the arr array.
-     * If there is no such element in the array, return -1.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [99, -7, 102], number = -7    ->   2
-     * arr = [5, -3, -4],   number = 10    ->  -1
-     */
-    public int findIndexOfNumber(int[] arr, int number) {
-
-    return 0;}
-
-    /**
-     * Return the new String[] array obtained from the arr array
-     * by reversing the order of the elements.
-     * <p>
-     * Example:
-     * <p>
-     * arr = ["Bob", "Nick"]               -> ["Nick", "Bob"]
-     * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
-     */
-    public String[] reverseArray(String[] arr) {
-
-        return null; }
-
-    /**
-     * Return new int[] array obtained from arr int[] array
-     * by choosing positive numbers only.
-     * P.S. 0 is not a positive number =)
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1,-2, 3]      -> [1, 3]
-     * arr = [-1, -2, -3]   -> []
-     * arr = [1, 2]         -> [1, 2]
-     */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int[] numbers = {2,-1,5,-4,3};
+        int[] nums= new int[10];
+        System.out.print(numbers);
 
-        return null;}
+
+
+
+        for (int n : numbers)
+        {
+            if(n>0)
+            {
+                nums = numbers;
+            }
+        }
+
+        return numbers;
+    }
+
+    public Object[] sortRaggedArray(int[][] arr) {
+        int matrix[][] = { { 39, 27, 11, 42 },
+                { 10, 93, 91, 90 },
+                { 54, 78, 56, 89 },
+                { 24, 64, 20, 65 } };
+        // Sort this matrix by 3rd Column
+        int col = 3;
+        //sortbyColumn(matrix, col - 1);
+
+        // Display the sorted Matrix
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.println();
+
+        }return sortRaggedArray(arr);
+    }
+}
+
+
+
+
+
+
+        //ArrayTasks arrayTasks=new ArrayTasks();
+        //arrayTasks.
+
+
 
     /**
-     * Return a sorted, ragged, two-dimensional int[][] array following these rules:
-     * Incoming one-dimensional arrays must be arranged in ascending order of their length;
-     * numbers in all one-dimensional arrays must be in ascending order.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
-     * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
+     * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
-    int[][] jagged = {
-            {5,10,2},
-            {4,8,12,25},
-            {3,2,1,4},
-            {100,2,0}
-    };
-    public int[][] sortRaggedArray(int [][] jagged) {
 
 
 
-        return null; }}
+
+
+
+
+
+
+
+
+
+
 
