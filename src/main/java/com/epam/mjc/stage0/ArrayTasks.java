2cf14354b466709bce39c5125a7c8c5fe70aea4d
Nodir Asomov <nasomov93@gmail.com>
2023-10-29 18:28:51 +0500

    hi

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -9,14 +9,19 @@ public class ArrayTasks {
 
 
     public static void main(String[] args) {
-        ArrayTasks arrayTasks= new ArrayTasks();
+        int[] arr = new int[]{1, 2, 3, 4, 5,6,7,8,9,10};
+        String[] arr3 = new String[]{"baba","zaza","mama","nanana"};
+        ArrayTasks arrayTasks = new ArrayTasks();
         arrayTasks.seasonsArray();
-        arrayTasks.generateNumbers(5);
+        arrayTasks.generateNumbers(4);
+        arrayTasks.totalSum(arr);
+       arrayTasks.findIndexOfNumber(arr, 4);
+       arrayTasks.reverseArray(arr3);
     }
 
-        public String[] seasonsArray() {
+    public String[] seasonsArray() {
 
-        String[] seasons=new String[5];
+        String[] seasons = new String[5];
         seasons[0] = " ";
         seasons[1] = "winter";
         seasons[2] = "spring";
@@ -25,105 +30,64 @@ public class ArrayTasks {
         for (int i = 1; i < 5; i++) {
             System.out.println(seasons[i]);
         }
-              return seasonsArray();}
+        return seasons;
+    }
 
-    public int[] generateNumbers(int length) { int[] intArr = {1,2,3,4,5,6};
-        for (int num : intArr) {
-            for (int i = 1; i <=length; i++) {
-                //System.out.print();
-                System.out.print("[");
-                for (int j = 1; j <=i; j++) {
-                    System.out.print(j+",");
-                }
-                System.out.print("]");
-                System.out.println();
+    public int[] generateNumbers(int length) {
+        int[] intArr = {1, 2, 3, 4, 5, 6};
+
+        for (int i = 1; i <= length; i++) {
+            //System.out.print();
+            System.out.print("[");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + ",");
             }
+            System.out.print("]");
+            System.out.println();
         }
-        return generateNumbers(5);
+        return intArr;
     }
 
-    public Object[] reverseArray(String[] arr) {
-        Integer[] intArray = {10,20,30,40,50,60,70,80,90};
-
-        //print array starting from first element
-        System.out.println("Original Array:");
-        for(int i=0;i<intArray.length;i++)
-            System.out.print(intArray[i] + "  ");
-
-        System.out.println();
-
-        //print array starting from last element
-        System.out.println("Original Array printed in reverse order:");
-        for(int i=intArray.length-1;i>=0;i--)
-            System.out.print(intArray[i] + "  ");
-        return reverseArray(arr);
-    }
 
     public int totalSum(int[] arr) {
-         arr = new int [] {1, 2, 3, 4, 5};
+        //arr = new int [] {1, 2, 3, 4, 5};
         int sum = 0;
         //Loop through the array to calculate sum of elements
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
-            //System.out.println(sum);
         }
         System.out.println(sum);
-        return totalSum(arr);
+        return sum;
     }
-
-
-
 
     public int findIndexOfNumber(int[] arr, int number) {
-        int target=102;
-        int[] numbers = {99, -7, 102};
-        for (int index = 0; index < numbers.length; index++) {
-        if (numbers[index] == target) {
-        return index;
-        }
-        }
-        return -1;
-        }
 
 
+        for (int i:arr) {
+            if (number == arr[i]) {
+                System.out.println(i);
+                //break;
+            }
+            System.out.println(-1);
+
+        }return -1;
+    }
+
+    public String[] reverseArray(String[] arr3) {
+        for(int i=0;i<arr3.length;i++)
+            System.out.print(arr3[i] + "  ");
+
+        System.out.println();
+        for(int i=arr3.length-1;i>=0;i--)
+            System.out.print(arr3[i] + "  ");
+     return arr3;
+}
 
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        int[] numbers = {2,-1,5,-4,3};
-        int[] nums= new int[10];
-        System.out.print(numbers);
+    return arr;}
 
-
-
-
-        for (int n : numbers)
-        {
-            if(n>0)
-            {
-                nums = numbers;
-            }
-        }
-
-        return numbers;
-    }
-
-    public Object[] sortRaggedArray(int[][] arr) {
-        int matrix[][] = { { 39, 27, 11, 42 },
-                { 10, 93, 91, 90 },
-                { 54, 78, 56, 89 },
-                { 24, 64, 20, 65 } };
-        // Sort this matrix by 3rd Column
-        int col = 3;
-        //sortbyColumn(matrix, col - 1);
-
-        // Display the sorted Matrix
-        for (int i = 0; i < matrix.length; i++) {
-
-            for (int j = 0; j < matrix[i].length; j++)
-                System.out.print(matrix[i][j] + " ");
-            System.out.println();
-
-        }return sortRaggedArray(arr);
-    }
+    public Object[] sortRaggedArray(int[][] arr) {return arr;
+    };
 }
 
 
@@ -131,14 +95,26 @@ public class ArrayTasks {
 
 
 
-        //ArrayTasks arrayTasks=new ArrayTasks();
-        //arrayTasks.
 
 
 
-    /**
-     * Return a String[] array that will list all the seasons of the year, starting with winter.
-     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
